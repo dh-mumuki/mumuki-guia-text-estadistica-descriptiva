720e3b669203ca35ac23831df2219296501f8471
andres@digitalhouse.com <andres@digitalhouse.com>
2019-04-17 16:47:01 +0000

    Mumuki Export on 2019-04-17 16:47:01 +0000

--- 00021_Únicos II/test.py
+++ 00021_Únicos II/test.py
@@ -1,4 +1,3 @@
-class Test(unittest.TestCase):
-
-  def test_description_example(self):
-    self.assertTrue(True)
+class TestFixtures(unittest.TestCase):
+   def test_unicos(self):
+      self.assertEquals(lista_de_unicos([1,2,3,3,3,3,4,5]),[1, 2, 3, 4, 5])
