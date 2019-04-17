e5e900467858b8df3cdc839cfb2dac2eb23e7d94
andres@digitalhouse.com <andres@digitalhouse.com>
2019-04-17 16:45:11 +0000

    Mumuki Export on 2019-04-17 16:45:11 +0000

--- 00014_Contador/test.py
+++ 00014_Contador/test.py
@@ -1,4 +1,5 @@
-class Test(unittest.TestCase):
-
-  def test_description_example(self):
-    self.assertTrue(True)
+class TestFixtures(unittest.TestCase):
+  def test_contador_2(self):
+    self.assertEquals(cuenta_cuatro([1, 4, 6, 4, 7]), 2)
+  def test_contado_3(self):
+    self.assertEquals(cuenta_cuatro([1, 4, 6, 4, 4]), 3)
