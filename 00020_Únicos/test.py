841c014dc89a919b5628d095017fc2869b514669
andres@digitalhouse.com <andres@digitalhouse.com>
2019-06-22 13:53:47 +0000

    Mumuki Export on 2019-06-22 13:53:47 +0000

--- 00020_Únicos/test.py
+++ 00020_Únicos/test.py
@@ -1,3 +1,3 @@
 class TestFixtures(unittest.TestCase):
   def test_unicos(self):
-    self.assertEquals(unicos([1,2,3,4,5]), [1,2,3,3,3,3,4,5])
+    self.assertEquals(unicos([1,2,3,3,3,3,4,5]), [1,2,3,4,5])
