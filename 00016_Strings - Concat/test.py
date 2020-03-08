bdb454712b7960ea16e390e5cd1afeeb17883289
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-08 18:48:28 +0000

    Mumuki Export on 2020-03-08 18:48:28 +0000

--- 00016_Strings - Concat/test.py
+++ 00016_Strings - Concat/test.py
@@ -2,4 +2,10 @@ class Test(unittest.TestCase):
 
   def test_description_example(self):
     ls = ["a", "s", "d", "f"]
-    self.assertEqual(concat_string(ls), "asdf")
+    self.assertEqual(
+      concat_string(ls),
+      "asdf",
+      "concat_string(['a', 's', 'd', 'f']) != 'asdf'"
+    )
+
+  def test_iterable(self):
