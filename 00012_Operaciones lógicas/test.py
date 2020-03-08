9f0ec78b144871b383f3ec5a47560f252d63fa57
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-08 18:06:35 +0000

    Mumuki Export on 2020-03-08 18:06:35 +0000

--- 00012_Operaciones lógicas/test.py
+++ 00012_Operaciones lógicas/test.py
@@ -1,12 +1,15 @@
 class TestFixtures(unittest.TestCase):
-  def test_1000(self):
-      self.assertFalse(near_thousand(1001))
+  def test_1001(self):
+      self.assertFalse(near_thousand(1001), "El 1001 NO debe pasar.")
 
   def test_900(self):
-      self.assertTrue(near_thousand(900))
+      self.assertTrue(near_thousand(900), "El 900 debe pasar.")
 
-  def test_800(self):
-      self.assertTrue(near_thousand(800))
+  def test_100(self):
+      self.assertTrue(near_thousand(100), "El 100 debe pasar.")
+
+  def test_50(self):
+      self.assertFalse(near_thousand(50), "El 50 NO debe pasar.")
 
   def test_values(self):
     function = lambda x: x >= 100 and x <= 1000
