b650fc69d0c3aa52758afedee236767212abe90b
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-08 19:10:18 +0000

    Mumuki Export on 2020-03-08 19:10:18 +0000

--- 00019_Primos/test.py
+++ 00019_Primos/test.py
@@ -1,5 +1,19 @@
 class TestFixtures(unittest.TestCase):
   def test_primos(self):
+    self.assertTrue(primos(1))
+    self.assertTrue(primos(2))
+    self.assertTrue(primos(3))
+    self.assertTrue(primos(5))
     self.assertTrue(primos(7))
+    self.assertTrue(primos(11))
+    self.assertTrue(primos(13))
+    self.assertTrue(primos(17))
+    self.assertTrue(primos(19))
+
   def test_no_primo(self):
+    self.assertFalse(primos(4))
+    self.assertFalse(primos(6))
+    self.assertFalse(primos(8))
     self.assertFalse(primos(9))
+    self.assertFalse(primos(10))
+    self.assertFalse(primos(15))
