755e2c210dc7aed3f12733661db05345a9db075c
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-08 18:59:16 +0000

    Mumuki Export on 2020-03-08 18:59:16 +0000

--- 00017_Trabajando con listas/test.py
+++ 00017_Trabajando con listas/test.py
@@ -1,5 +1,7 @@
 class Test248(unittest.TestCase):
   def test_248(self):
     self.assertEquals(stop_248([2, 3, 4, 248]), [2, 4])
+    self.assertEquals(stop_248([2, 3, 4, 248, 6, 8, 10]), [2, 4])
       
-  def test_no_evens(self):  
+  def test_no_evens(self):
+    self.assertEquals(stop_248([3, 5, 7]), [])
