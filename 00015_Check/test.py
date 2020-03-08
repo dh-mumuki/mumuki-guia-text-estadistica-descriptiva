e9779b307b18b8232ebf5d5f1829aae8f1f1522e
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-08 18:30:22 +0000

    Mumuki Export on 2020-03-08 18:30:22 +0000

--- 00015_Check/test.py
+++ 00015_Check/test.py
@@ -2,4 +2,6 @@ class TestCheck(unittest.TestCase):
 
   def test_four_in_list(self):
     self.assertTrue(check(4, [3, 2, 4, 5]), "check(4, [3, 2, 4, 5]) != True")
+    
+  def test_four_not_in_list(self):
     self.assertEqual(check(4, [3, 2, 5]), [3, 2, 5, 4], "check(4, [3, 2, 5]) != [3, 2, 5, 4]")
