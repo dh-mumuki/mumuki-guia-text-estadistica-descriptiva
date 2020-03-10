2857d62fdfe0b62c448d3561d58982767a2747f2
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-10 01:39:57 +0000

    Mumuki Export on 2020-03-10 01:39:57 +0000

--- 00018_Repetidos/test.py
+++ 00018_Repetidos/test.py
@@ -1,7 +1,7 @@
 class TestRepetidos(unittest.TestCase):
-   def test_repetidos(self):
-      self.assertTrue(repetidos([1, 1, 2]))
-      self.assertTrue(repetidos([1, 2, 3, 4, 2]), "repetidos([1, 2, 3, 4, 2]) != True")
+  def test_repetidos(self):
+    self.assertTrue(repetidos([1, 1, 2]))
+    self.assertTrue(repetidos([1, 2, 3, 4, 2]), "repetidos([1, 2, 3, 4, 2]) != True")
 
-    def test_nonrep(self):
-      self.assertFalse(repetidos([1, 2, 3]), "repetidos([1, 2, 3]) != False")
+  def test_nonrep(self):
+    self.assertFalse(repetidos([1, 2, 3]), "repetidos([1, 2, 3]) != False")
