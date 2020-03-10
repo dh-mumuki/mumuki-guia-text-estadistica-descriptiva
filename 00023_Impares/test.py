b1fe0b78caf154b60c2364a9897e0d49f04af09a
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-10 02:00:13 +0000

    Mumuki Export on 2020-03-10 02:00:13 +0000

--- 00023_Impares/test.py
+++ 00023_Impares/test.py
@@ -1,4 +1,4 @@
 class Test(unittest.TestCase):
 
   def test_description_example(self):
-    self.assertTrue(impares(21), [1,3,5,7,9,11,13,15,17,19])
+    self.assertEqual(impares(21), [1,3,5,7,9,11,13,15,17,19])
