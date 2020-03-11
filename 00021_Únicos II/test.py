653a9cd66fea0231d179a39bf8df3c24e24cf16d
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 15:30:38 +0000

    Mumuki Export on 2020-03-11 15:30:38 +0000

--- 00021_Únicos II/test.py
+++ 00021_Únicos II/test.py
@@ -4,3 +4,12 @@ class TestUnicosDos(unittest.TestCase):
       
    def test_order(self):
       self.assertEquals(lista_de_unicos([1, 3, 3, 3, 3, 2, 4, 5]), [1, 2, 3, 4, 5])
+      
+  def test_n_args(self):
+    try:
+      with self.assertRaises(TypeError):
+        lista_de_unicos()
+      with self.assertRaises(TypeError):
+        lista_de_unicos([2, 4, 6, 248, 4], 2)
+    except:
+      raise ValueError("Revisar numero de argumentos de funcion.")
