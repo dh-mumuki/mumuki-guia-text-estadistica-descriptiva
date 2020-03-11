b47b630da8126aa0b215e4a567d11ec81294818c
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 14:47:03 +0000

    Mumuki Export on 2020-03-11 14:47:03 +0000

--- 00023_Impares/test.py
+++ 00023_Impares/test.py
@@ -7,4 +7,4 @@ class TestImpares(unittest.TestCase):
     self.assertEqual(impares(18), [1,3,5,7,9,11,13,15,17])
     
   def test_listcomp(self):
-    self.assertRegexpMatches(inspect.getsource(impares), r"\[.*for.*in.*\]", msg="El ejercicio debe resolverse con una lista por comprensión.")
+    self.assertRegexpMatches(inspect.getsource(impares), r"\[.*for.*in.*\]", msg="El ejercicio debe resolverse con una lista por comprension.")
