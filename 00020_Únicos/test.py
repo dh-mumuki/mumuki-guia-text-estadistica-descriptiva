1281481a9945fd1cbafa8d334a5cb0c34683903c
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 15:28:52 +0000

    Mumuki Export on 2020-03-11 15:28:52 +0000

--- 00020_Únicos/test.py
+++ 00020_Únicos/test.py
@@ -8,3 +8,12 @@ class TestFixtures(unittest.TestCase):
 
   def test_disorder(self):
     self.assertEquals(lista_de_unicos([1, 3, 3, 3, 3, 2, 4, 5]), [1, 3, 2, 4, 5])
+
+  def test_n_args(self):
+    try:
+      with self.assertRaises(TypeError):
+        lista_de_unicos()
+      with self.assertRaises(TypeError):
+        lista_de_unicos([2, 4, 6, 248, 4], 2)
+    except:
+      raise ValueError("Revisar numero de argumentos de funcion.")
