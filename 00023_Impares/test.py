99e3aedebce3f30e78fad10638eed5618f390ac5
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 14:41:54 +0000

    Mumuki Export on 2020-03-11 14:41:54 +0000

--- 00023_Impares/test.py
+++ 00023_Impares/test.py
@@ -1,5 +1,10 @@
-class Test(unittest.TestCase):
+import inspect
+
+class TestImpares(unittest.TestCase):
 
   def test_description_example(self):
     self.assertEqual(impares(21), [1,3,5,7,9,11,13,15,17,19])
     self.assertEqual(impares(18), [1,3,5,7,9,11,13,15,17])
+    
+  def test_listcomp(self):
+    self.assertRegex(inspect.getsource(impares), r"\[.*for.*in.*\]")
