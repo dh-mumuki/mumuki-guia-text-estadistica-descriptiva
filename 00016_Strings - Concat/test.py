6e81ebb970f24215ec284852ad854a75198728d7
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 15:15:30 +0000

    Mumuki Export on 2020-03-11 15:15:30 +0000

--- 00016_Strings - Concat/test.py
+++ 00016_Strings - Concat/test.py
@@ -11,3 +11,12 @@ class TestConcat(unittest.TestCase):
   def test_not_iterable(self):
     with self.assertRaises(TypeError, msg="Tiene que fallar con un número"):
       concat_string(234)
+
+  def test_n_args(self):
+    try:
+      with self.assertRaises(TypeError, msg="La funcion no debe correr sin argumentos"):
+        cuenta_cuatro()
+      with self.assertRaises(TypeError, msg="La funcion no debe correr con dos argumentos"):
+        cuenta_cuatro(["a", "s", "d"], 2)
+    except:
+      raise ValueError("Revisar numero de argumentos de funcion.")
