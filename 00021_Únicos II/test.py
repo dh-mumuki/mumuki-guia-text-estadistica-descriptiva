665196c6c98465558341f7827232fc3d73cfb3f2
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 15:31:13 +0000

    Mumuki Export on 2020-03-11 15:31:13 +0000

--- 00021_Únicos II/test.py
+++ 00021_Únicos II/test.py
@@ -1,9 +1,9 @@
 class TestUnicosDos(unittest.TestCase):
-   def test_unicos(self):
-      self.assertEquals(lista_de_unicos([1, 2, 3, 3, 3, 3, 4, 5]), [1, 2, 3, 4, 5])
+  def test_unicos(self):
+    self.assertEquals(lista_de_unicos([1, 2, 3, 3, 3, 3, 4, 5]), [1, 2, 3, 4, 5])
       
-   def test_order(self):
-      self.assertEquals(lista_de_unicos([1, 3, 3, 3, 3, 2, 4, 5]), [1, 2, 3, 4, 5])
+  def test_order(self):
+    self.assertEquals(lista_de_unicos([1, 3, 3, 3, 3, 2, 4, 5]), [1, 2, 3, 4, 5])
       
   def test_n_args(self):
     try:
