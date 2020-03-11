5280be8c5961342cbed1c1c0546c3bef211f1bec
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 15:24:29 +0000

    Mumuki Export on 2020-03-11 15:24:29 +0000

--- 00018_Repetidos/test.py
+++ 00018_Repetidos/test.py
@@ -5,3 +5,12 @@ class TestRepetidos(unittest.TestCase):
 
   def test_nonrep(self):
     self.assertFalse(repetidos([1, 2, 3]), "repetidos([1, 2, 3]) != False")
+
+  def test_n_args(self):
+    try:
+      with self.assertRaises(TypeError):
+        repetidos()
+      with self.assertRaises(TypeError):
+        repetidos([2, 4, 6, 248, 4], 2)
+    except:
+      raise ValueError("Revisar numero de argumentos de funcion.")
