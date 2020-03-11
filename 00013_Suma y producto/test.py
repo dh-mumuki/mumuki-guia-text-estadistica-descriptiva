0ae9e89e19991b77783a10dbc59f208b726c4de7
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 14:59:07 +0000

    Mumuki Export on 2020-03-11 14:59:07 +0000

--- 00013_Suma y producto/test.py
+++ 00013_Suma y producto/test.py
@@ -4,3 +4,9 @@ class TestFixtures(unittest.TestCase):
 
   def test_suma_iguales(self):
     self.assertEquals(suma_tres(4, 4, 4), 36)
+
+  def test_n_args(self):
+    with self.assertRaises(TypeError, msg="La funcion no debe correr sin argumentos"):
+      near_thousand()
+    with self.assertRaises(TypeError, msg="La funcion no debe correr con cuatro argumentos"):
+      near_thousand(1, 2, 3, 4)
