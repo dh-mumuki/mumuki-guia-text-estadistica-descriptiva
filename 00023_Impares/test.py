096abcd2c569fd2814006eeff6dde8f7d02c6681
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 15:34:16 +0000

    Mumuki Export on 2020-03-11 15:34:16 +0000

--- 00023_Impares/test.py
+++ 00023_Impares/test.py
@@ -8,3 +8,12 @@ class TestImpares(unittest.TestCase):
     
   def test_listcomp(self):
     self.assertRegexpMatches(inspect.getsource(impares), r"\[.*for.*in.*\]", msg="El ejercicio debe resolverse con una lista por comprension.")
+
+  def test_n_args(self):
+    try:
+      with self.assertRaises(TypeError):
+        impares()
+      with self.assertRaises(TypeError):
+        impares(3, 2)
+    except:
+      raise ValueError("Revisar numero de argumentos de funcion.")
