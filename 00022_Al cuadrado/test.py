e8c3c29457fda627771c4a173d48a7d1477750ae
mrios@digitalhouse.com <mrios@digitalhouse.com>
2020-03-11 15:32:58 +0000

    Mumuki Export on 2020-03-11 15:32:58 +0000

--- 00022_Al cuadrado/test.py
+++ 00022_Al cuadrado/test.py
@@ -1,3 +1,10 @@
 class TestFixtures(unittest.TestCase):
   def test_cuadrados(self):
     self.assertTrue(cuadrados() == [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484, 529, 576, 625, 676, 729, 784, 841, 900])
+
+  def test_n_args(self):
+    try:
+      with self.assertRaises(TypeError):
+        cuadrados(1)
+    except:
+      raise ValueError("Revisar numero de argumentos de funcion.")
